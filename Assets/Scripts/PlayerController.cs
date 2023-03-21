26c677ce12f73281867a579b3b14e991b12f0cdf
Guilherme Fugimoto <guilherme.kfugimoto@gmail.com>
2023-03-20 22:17:41 -0300

    Animação Idle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,45 +7,61 @@ public class PlayerController : MonoBehaviour
     private string moveInputAxis = "Vertical";
     private string turnInputAxis = "Horizontal";
     public float moveSpeed = 1;
+    private bool grounded;
 
-    Animator anim;
+    public Animator anim;
 
     public SpriteRenderer playerSprite;
     Vector3 movement;
    
     void Start()
     {
-        anim = gameObject.GetComponent<Animator>();
+        
     }
 
     
     void Update()
     {
+        Move();
+       
+    }
+
+    private void Move()
+    {
         float moveAxis = Input.GetAxis(moveInputAxis);
         float turnAxis = Input.GetAxis(turnInputAxis);
 
         movement = new Vector3(turnAxis, 0, moveAxis);
-        //movement = Vector3.ClampMagnitude(movement, 1);
-        movement = movement.normalized;
+        movement = Vector3.ClampMagnitude(movement, 1);
+        //movement = movement.normalized;
 
-        Move();
 
-        //Controla a direção do sprite quando o player está se movendo
+        transform.Translate(movement * moveSpeed);
+
+
+        if (moveAxis != 0)
+        {
+            anim.SetBool("animMove", true);
+            Debug.Log("teste");
+        }
+        else if (moveAxis == 0)
+        {
+            anim.SetBool("animMove", false);
+        }
+        
+
+         //Controla a direção do sprite quando o player está se movendo
         if (turnAxis != 0 && turnAxis < 0)
         {
             playerSprite.flipX = true;
+            anim.SetBool("animMove", true);
         }
         else if (turnAxis != 0 && turnAxis > 0)
         {
             playerSprite.flipX = false;
+            anim.SetBool("animMove", true);
         }
         
-
-    }
-    
-    private void Move()
-    {
-        transform.Translate(movement * moveSpeed);
     }
     
 
---
+++ Assets/SmoothCamera/Scripts/SmoothCamera.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCamera : MonoBehaviour
+{
+    [Header("Reference")]
+    [SerializeField] private Transform lookAt;
+
+    [Header("Tweaks")]  
+    [SerializeField] private Vector3 offset = new Vector3(0, 10, -10);
+
+    [Header("Bounds")]
+    [SerializeField] private bool enabledBoundX = true;
+    [SerializeField] private float boundX = 1.0f;
+
+    [Header("Smooth")]
+    [SerializeField] private bool smooth = true;
+    [SerializeField] private float smoothSpeed = 5;
+
+    [Header("Fields")]
+    private Vector3 desiredPosition;
+
+    // LateUpdate, as we have to make sure the camera moves AFTER the player
+    private void LateUpdate()
+    {
+        desiredPosition = lookAt.position + offset;
+
+        if (enabledBoundX)
+        {
+            // This is to check if we're inside the bounds on the X axis
+            float deltaX = lookAt.position.x - transform.position.x;
+            if (Mathf.Abs(deltaX) > boundX)
+            {
+                if (deltaX > 0)
+                {
+                    desiredPosition.x = lookAt.position.x - boundX;
+                }
+                else
+                {
+                    desiredPosition.x = lookAt.position.x + boundX;
+                }
+            }
+            else
+            {
+                desiredPosition.x = lookAt.position.x - deltaX;
+            }
+        }
+
+        if (smooth)
+            transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * smoothSpeed);
+        else
+            transform.position = desiredPosition;
+    }
+}
