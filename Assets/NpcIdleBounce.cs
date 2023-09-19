827884f61dae5bc74a04ae13e41858fc8da8003f
Gezuiz <69606836+Gezuiz@users.noreply.github.com>
2023-09-19 17:01:04 -0300

    Npcs & Idle

---
+++ Assets/NpcIdleBounce.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcIdleBounce : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Vector3 ScaleChange;
+    [SerializeField] bool Deflate; 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Deflate == false)
+        {
+            gameObject.transform.localScale += ScaleChange;
+        }
+        
+        if (Deflate == true)
+        {
+            gameObject.transform.localScale -= ScaleChange;
+        }
+
+        if (gameObject.transform.localScale.y >= 5.85)
+        {
+            Deflate = true;
+        }
+
+        if (gameObject.transform.localScale.y <= 5.75)
+        {
+            Deflate = false;   
+        }
+    }
+}
